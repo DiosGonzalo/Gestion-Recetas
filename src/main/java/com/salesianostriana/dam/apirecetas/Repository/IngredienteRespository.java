caa92a5c6e027b533a1ef37db02f6a1eed880664
DiosGonzalo <gdiosfiestas@gmail.com>
2025-11-26 12:24:35 +0100

    service creado y medio controiler

--- src/main/java/com/salesianostriana/dam/apirecetas/Repository/IngredienteRespository.java
+++ src/main/java/com/salesianostriana/dam/apirecetas/Repository/IngredienteRespository.java
@@ -1,7 +1,13 @@
 package com.salesianostriana.dam.apirecetas.Repository;
 
+import com.salesianostriana.dam.apirecetas.models.Categoria;
 import com.salesianostriana.dam.apirecetas.models.Ingrediente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IngredienteRespository extends JpaRepository<Ingrediente,Long> {
+    boolean existsByNombre(String nombre);
+
+    Optional<Ingrediente> findByNombre(String nombre);
 }
